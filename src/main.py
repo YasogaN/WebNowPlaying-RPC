e6bde30eb389b962d473d54f43baa174e630d892
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2024-01-07 01:32:15 +0530

    Refactor clear function and update console title

--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ from colorama import Fore as color #for colorful text
 import os
 import subprocess
 import psutil
+import ctypes
 
-# Clear the terminal
+# clear_console the terminal
 # for windows
 
-def clear():
+def clear_console():
     if os.name == 'nt':
         _ = os.system('cls')
     
@@ -15,9 +16,10 @@ def clear():
     else:
         _ = os.system('clear')
 
-clear()
+clear_console()
 
 # welcome message
+ctypes.windll.kernel32.SetConsoleTitleW("WebNowPlayingRPC - CLI")
 print(color.BLUE + "Welcome to the WebNowPlaying Discord Rich Presence CLI!")
 
 # define the exit function
@@ -49,19 +51,20 @@ while True:
                 file.close()
             if psutil.pid_exists(pid):
                 print("Task with PID", pid, "exists.")
+                clear_console()
                 print(color.RED + "RPC is already running!")
                 print(color.BLUE + "New RPC will not be started!")
             else:
                 print(color.RED + "!! RPC was shutdown unexpectedly !!")
                 print(color.BLUE + "Starting the RPC...")
                 subprocess.Popen(["pythonw", "src/script.pyw"], creationflags=subprocess.CREATE_NO_WINDOW)
-                clear()
+                clear_console()
                 print(color.GREEN + "Done!")
         else:
             # Start the process
             print(color.BLUE + "Starting the RPC...")
             subprocess.Popen(["pythonw", "src/script.pyw"], creationflags=subprocess.CREATE_NO_WINDOW)
-            clear()
+            clear_console()
             print(color.GREEN + "Done!")
 
     # exit the rpc client
@@ -77,11 +80,11 @@ while True:
             # Kill the process
             os.system("taskkill /PID " + str(pid) + " /F")
             os.remove("src/pid.txt")
-            clear()
+            clear_console()
             print(color.GREEN + "Done!")
 
         else:
-            clear()
+            clear_console()
             print(color.RED + "RPC is not running!")
     
     # exit the program
@@ -93,10 +96,10 @@ while True:
         print(color.BLUE + "Creating task...")
         #ask for admin rights and create task scheduler task
         os.system("powershell -Command \"Start-Process cmd -Verb RunAs -ArgumentList '/c schtasks /create /sc onlogon /tn WebNowPlaying /delay 0000:59 /tr " + os.getcwd() + "\\src\\script.pyw'\"")
-        clear()
+        clear_console()
         print (color.GREEN + "Done!")
     
     # invalid choice
     else:
         print(color.RED + "Invalid choice!")
-        clear()
+        clear_console()
