212d28a63d19b4b42e0c55d6cfee68e4fee14414
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2024-01-24 01:04:13 +0530

    Remove Experimental Feature Because It doesnt work

--- src/dsdk/event.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-    
-def bindEvents(structure, *methods):
-    contents = structure()
-    for index, (name, func) in enumerate(structure._fields_):
-        setattr(contents, name, func(methods[index]))
-        
-    pointer = ctypes.pointer(contents)
-    return pointer
